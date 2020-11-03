03b315fd0a6bde4906169218cd97bba1771a4f36
Hovhannesmn <hovhannesmn@gmail.com>
2020-11-03 21:23:10 +0400

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 4000;
 
+cors();
 app.listen(port, function () {});
 
 //Classes to import for wolfram api
